47a87e0ce144e89814bbe71c7184a5974321ade6
TheRitwik <ritwikkumar.satpati@gmail.com>
2024-05-06 15:07:34 +0530

    bug fix in nodejs-expressjs-mongodb-esmodule-javascript & nodejs-expressjs-mongodb-multiple-connection-esmodule-javascript;

--- nodejs-expressjs-mongodb-multiple-connection-esmodule-javascript/src/utils/asyncHandler.js
+++ nodejs-expressjs-mongodb-multiple-connection-esmodule-javascript/src/utils/asyncHandler.js
@@ -1,23 +1,24 @@
-// // Without Promise.resolve
-// const asyncHandler = (fn) => async (req, res, next) => {
-//   try {
-//     await fn(req, res, next);
-//   } catch (err) {
-//     res.status(err.code || 500).json({
-//       success: false,
-//       message: err.message,
-//     });
-//   }
-// };
-
-// export default asyncHandler
-
-
-// With Promise.resolve
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-    };
+// // Without function
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch (err) {
+        console.error(err.stack);
+        res.status(err.statusCode || 500).json({
+            success: false,
+            message: err.message,
+        });
+    }
 };
 
 export { asyncHandler };
+
+
+// // With Promise.resolve
+// const asyncHandler = (requestHandler) => {
+//     return (req, res, next) => {
+//         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+//     };
+// };
+
+// export { asyncHandler };
